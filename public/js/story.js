286e52e41a49d5461ab87e3c3216db486f22388e
tiitustelke <tiitus.telke@metropolia.fi>
2021-04-28 18:29:50 +0300

    Update js file for story

--- public/js/story.js
+++ public/js/story.js
@@ -3,3 +3,35 @@ jQuery(document).ready(function($) {
     window.location = $(this).data("href");
   });
 });
+const url = 'https://localhost:8000';
+
+const table = document.querySelector('.documents');
+
+const addPhotos = (photos) => {
+
+}
+
+const addDocuments = (documents) => {
+  table.innerHTML = `<tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Upload date</th>
+                     </tr>`;
+
+  documents.forEach((doc) => {
+    //create table rows
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    tr.innerHTML += `<td>${doc.name}</td>
+                     <td>${}</td>`
+    td.textContent = doc.name;
+  });
+
+
+}
+
+const getDocuments = async () => {
+  const response = await fetch(url + '/document' + window.location.path);
+}
+
+getDocuments();
